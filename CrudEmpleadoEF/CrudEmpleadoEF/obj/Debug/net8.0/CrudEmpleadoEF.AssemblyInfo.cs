881dd08d4de320a36f72b71b5049f598d752068a
Tomas Cienega <tomascienega0206@gmail.com>
2026-02-06 20:57:53 -0600

    ef

--- CrudEmpleadoEF/CrudEmpleadoEF/obj/Debug/net8.0/CrudEmpleadoEF.AssemblyInfo.cs
+++ CrudEmpleadoEF/CrudEmpleadoEF/obj/Debug/net8.0/CrudEmpleadoEF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEmpleadoEF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447db2165e5c47c1d9ece9f2cb7074a5927533ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f75b433f47842cf1f7120a45bceebe3b4d202322")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEmpleadoEF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEmpleadoEF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
